03b3e0e4a3c9489ac61c9dc6662e162dff693d32
Jan Weidner <jw3126@gmail.com>
2017-12-06 12:59:07 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Setfield
+
+## Usage
+```julia
+t = T(1,1)
+@set t.a = 5
+# T(5, 2)
+
+t = T(1, T(2, T(3,3)))
+@set t.b.b = 42
+# T(1, T(2, 42))
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using Setfield
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+struct T
+    a
+    b
+end
+
+@testset "@set" begin
+    t = T(1,2)
+    @test T(5,2) == @set t.a = 5
+    @test T(1, T(1,2)) == @set t.b = T(1,2)
+    @test_throws ArgumentError @set t.c = 3
+    @test t == T(1,2)
+    
+    t2 = T(1, T(2, T(T(4,4),3)))
+    @test T(1, T(2, 4)) == @set t2.b.b = 4
+    @test T(1, T(2, T(T(5, 4), 3))) == @set t2.b.b.a.a = 5
+    @test_throws ArgumentError @set t2.b.b.a.a.a = 3
+    @test t2 == T(1, T(2, T(T(4,4),3)))
+end
