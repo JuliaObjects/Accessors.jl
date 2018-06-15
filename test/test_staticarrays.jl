e36f82a7d164bb7d3b3da52613d68fc96f41a40a
Takafumi Arakaki <takafumi.a@gmail.com>
2018-06-15 08:01:18 -0700

    Fix test_staticarrays.jl (#33)

--- test/test_staticarrays.jl
+++ test/test_staticarrays.jl
@@ -3,8 +3,8 @@ using StaticArrays
 obj = StaticArrays.@SMatrix [1 2; 3 4]
 l = @lens _[2,1]
 @test get(l, obj) == 3
-@test_broken set(l, obj, 5) == StaticArrays.@SMatrix [1 2; 5 4]
-@test_broken setindex(obj, 5, 2, 1) == StaticArrays.@SMatrix [1 2; 5 4]
+@test set(l, obj, 5) == StaticArrays.@SMatrix [1 2; 5 4]
+@test setindex(obj, 5, 2, 1) == StaticArrays.@SMatrix [1 2; 5 4]
 
 v = @SVector [1,2,3]
 @test (@set v[1] = 10) === @SVector [10,2,3]
