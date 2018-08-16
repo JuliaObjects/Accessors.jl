50625dbef50cb6dcac738345d027615f49c2aa4f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-15 18:22:16 -0700

    Deploy document build with 1.0

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -2,7 +2,7 @@ using Documenter
 
 deploydocs(
     repo = "github.com/jw3126/Setfield.jl.git",
-    julia = "0.6",
+    julia = "1.0",
     target = "build",
     deps   = nothing,
     make   = nothing
