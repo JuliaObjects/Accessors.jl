b83b41545fbc7aa53c662961d146e5d509556219
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-05 17:50:56 -0700

    Cat build.jl and deploy.jl to make.jl
    Now that we are not separating build and deploy, this is unnecessary.
    DocumentationGenerator.jl looks at the code in make.jl to generate
    documentation hosted on https://pkg.julialang.org.  So, it's good to
    keep things normal.

--- docs/deploy.jl
+++
@@ -1,9 +0,0 @@
-using Documenter
-
-deploydocs(
-    repo = "github.com/jw3126/Setfield.jl.git",
-    julia = "1.0",
-    target = "build",
-    deps   = nothing,
-    make   = nothing
-)
--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,20 @@
-include("build.jl")
-include("deploy.jl")
+using Setfield, Documenter
+
+makedocs(
+         format = :html,
+         modules = [Setfield],
+         sitename = "Setfield.jl",
+         pages = [
+            "Introduction" => "intro.md",
+            "Docstrings" => "index.md",
+            ],
+        strict = true,  # to exit with non-zero code on error
+        )
+
+deploydocs(
+    repo = "github.com/jw3126/Setfield.jl.git",
+    julia = "1.0",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
+)
