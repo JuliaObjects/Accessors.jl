e12882df84f7cfd8987ed47a5038a3f7e27d90ec
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-09 23:10:45 -0700

    Add more `FunctionLens`es

--- src/functionlenses.jl
+++ src/functionlenses.jl
@@ -1,2 +1,14 @@
 set(obj, ::FunctionLens{last}, val) = @set obj[length(obj)] = val
 set(obj, ::FunctionLens{first}, val) = @set obj[1] = val
+
+set(obj::Array, ::FunctionLens{eltype}, T::Type) = collect(T, obj)
+set(::Type{<:Array{<:Any, N}}, ::FunctionLens{eltype}, ::Type{T}) where {N, T} =
+    Array{T, N}
+
+set(obj::Dict, l::FunctionLens, T::Type) = set(typeof(obj), l, T)(obj)
+set(::Type{<:Dict}, ::FunctionLens{eltype}, ::Type{Pair{K, V}}) where {K, V} =
+    Dict{K, V}
+set(::Type{<:Dict{<:Any,V}}, ::FunctionLens{keytype}, ::Type{K}) where {K, V} =
+    Dict{K, V}
+set(::Type{<:Dict{K}}, ::FunctionLens{valtype}, ::Type{V}) where {K, V} =
+    Dict{K, V}
--- test/test_functionlenses.jl
+++ test/test_functionlenses.jl
@@ -24,4 +24,35 @@ end
     @test (@set last(obj2.a).b = '2') === (a=(1, (b='2',)), c=3)
 end
 
+@testset "eltype(::Type{<:Array})" begin
+    obj = Vector{Int}
+    obj2 = @set eltype(obj) = Float64
+    @test obj2 === Vector{Float64}
+end
+
+@testset "eltype(::Array)" begin
+    obj = [1, 2, 3]
+    obj2 = @set eltype(obj) = Float64
+    @test eltype(obj2) == Float64
+    @test obj == obj2
+end
+
+@testset "(key|val|el)type(::Type{<:Dict})" begin
+    obj = Dict{Symbol, Int}
+    @test (@set keytype(obj) = String) === Dict{String, Int}
+    @test (@set valtype(obj) = String) === Dict{Symbol, String}
+    @test (@set eltype(obj) = Pair{String, Any}) === Dict{String, Any}
+
+    obj2 = Dict{Symbol, Dict{Int, Float64}}
+    @test (@set keytype(valtype(obj2)) = String) === Dict{Symbol, Dict{String, Float64}}
+    @test (@set valtype(valtype(obj2)) = String) === Dict{Symbol, Dict{Int, String}}
+end
+
+@testset "(key|val|el)type(::Dict)" begin
+    obj = Dict(1 => 2)
+    @test typeof(@set keytype(obj) = Float64) === Dict{Float64, Int}
+    @test typeof(@set valtype(obj) = Float64) === Dict{Int, Float64}
+    @test typeof(@set eltype(obj) = Pair{UInt, Float64}) === Dict{UInt, Float64}
+end
+
 end  # module
