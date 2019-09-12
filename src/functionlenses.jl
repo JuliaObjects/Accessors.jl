d1d46f98ee9ee381a07ae4484476c3a4585f53c0
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-11 22:10:15 -0700

    Use at-lens when defining set for FunctionLens

--- src/functionlenses.jl
+++ src/functionlenses.jl
@@ -1,14 +1,14 @@
-set(obj, ::FunctionLens{last}, val) = @set obj[lastindex(obj)] = val
-set(obj, ::FunctionLens{first}, val) = @set obj[firstindex(obj)] = val
+set(obj, ::typeof(@lens last(_)), val) = @set obj[lastindex(obj)] = val
+set(obj, ::typeof(@lens first(_)), val) = @set obj[firstindex(obj)] = val
 
-set(obj::Array, ::FunctionLens{eltype}, T::Type) = collect(T, obj)
-set(::Type{<:Array{<:Any, N}}, ::FunctionLens{eltype}, ::Type{T}) where {N, T} =
+set(obj::Array, ::typeof(@lens eltype(_)), T::Type) = collect(T, obj)
+set(::Type{<:Array{<:Any, N}}, ::typeof(@lens eltype(_)), ::Type{T}) where {N, T} =
     Array{T, N}
 
 set(obj::Dict, l::FunctionLens, T::Type) = set(typeof(obj), l, T)(obj)
-set(::Type{<:Dict}, ::FunctionLens{eltype}, ::Type{Pair{K, V}}) where {K, V} =
+set(::Type{<:Dict}, ::typeof(@lens eltype(_)), ::Type{Pair{K, V}}) where {K, V} =
     Dict{K, V}
-set(::Type{<:Dict{<:Any,V}}, ::FunctionLens{keytype}, ::Type{K}) where {K, V} =
+set(::Type{<:Dict{<:Any,V}}, ::typeof(@lens keytype(_)), ::Type{K}) where {K, V} =
     Dict{K, V}
-set(::Type{<:Dict{K}}, ::FunctionLens{valtype}, ::Type{V}) where {K, V} =
+set(::Type{<:Dict{K}}, ::typeof(@lens valtype(_)), ::Type{V}) where {K, V} =
     Dict{K, V}
