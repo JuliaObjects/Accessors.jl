bff1c525afbd065c524f86fb0674d17c304c29ee
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-09 10:12:31 -0800

    Allow changing type via Setfield.setindex

--- src/setindex.jl
+++ src/setindex.jl
@@ -2,12 +2,21 @@ Base.@propagate_inbounds function setindex(args...)
     Base.setindex(args...)
 end
 
-for T in [:Array, :Dict]
-    @eval begin
-        Base.@propagate_inbounds function setindex(o::$T, args...)
-            new = copy(o)
-            setindex!(new, args...)
-            new
-        end
+Base.@propagate_inbounds function setindex(xs::AbstractArray, v, I...)
+    T = promote_type(eltype(xs), typeof(v))
+    ys = similar(xs, T)
+    if eltype(xs) !== Union{}
+        copy!(ys, xs)
     end
+    ys[I...] = v
+    return ys
+end
+
+Base.@propagate_inbounds function setindex(d0::AbstractDict, v, k)
+    K = promote_type(keytype(d0), typeof(k))
+    V = promote_type(valtype(d0), typeof(v))
+    d = empty(d0, K, V)
+    copy!(d, d0)
+    d[k] = v
+    return d
 end
--- test/test_setindex.jl
+++ test/test_setindex.jl
@@ -2,6 +2,19 @@ module TestSetindex
 using Setfield
 using Test
 
+"""
+    ==ₜ(x, y)
+
+Check that _type_ and value of `x` and `y` are equal.
+"""
+==ₜ(_, _) = false
+==ₜ(x::T, y::T) where T = x == y
+
+@testset "==ₜ" begin
+    @test 1 ==ₜ 1
+    @test !(1.0 ==ₜ 1)
+end
+
 @testset "setindex" begin
     arr = [1,2,3]
     @test_throws MethodError Base.setindex(arr, 10, 1)
@@ -9,6 +22,7 @@ using Test
     @test arr == [1,2,3]
     @test @set(arr[1] = 10) == [10, 2, 3]
     @test arr == [1,2,3]
+    @test Setfield.setindex(arr, 10.0, 1) ==ₜ Float64[10.0, 2.0, 3.0]
 
     d = Dict(:a => 1, :b => 2)
     @test_throws MethodError Base.setindex(d, 10, :a)
@@ -16,6 +30,8 @@ using Test
     @test d == Dict(:a => 1, :b => 2)
     @test @set(d[:a] = 10) == Dict(:a=>10, :b=>2)
     @test d == Dict(:a => 1, :b => 2)
+    @test Setfield.setindex(d, 30, "c") ==ₜ Dict(:a=>1, :b=>2, "c"=>30)
+    @test Setfield.setindex(d, 10.0, :a) ==ₜ Dict(:a=>10.0, :b=>2.0)
 end
 
 end
