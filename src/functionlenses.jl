2db94566c1e3184d7c33fe283ea0b5e302376bbe
Alexander Plavin <alexander@plav.in>
2022-06-02 22:20:37 +0300

    add Date/Time setters

--- src/functionlenses.jl
+++ src/functionlenses.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra: norm, normalize
+using Dates
 
 set(obj, ::typeof(last), val) = @set obj[lastindex(obj)] = val
 set(obj, ::typeof(first), val) = @set obj[firstindex(obj)] = val
@@ -53,3 +54,28 @@ set(x,       ::typeof(imag), y) = real(x) + im*y
 
 set(arr, ::typeof(normalize), val) = norm(arr) * val
 set(arr, ::typeof(norm), val)      = val/norm(arr) * arr # should we check val is positive?
+
+################################################################################
+##### dates
+################################################################################
+set(x::DateTime, ::Type{Date}, y) = DateTime(y, Time(x))
+set(x::DateTime, ::Type{Time}, y) = DateTime(Date(x), y)
+set(x::T, ::Type{T}, y) where {T <: Union{Date, Time}} = y
+
+set(x::Date, ::typeof(year),                    y) = Date(y,       month(x), day(x))
+set(x::Date, ::typeof(month),                   y) = Date(year(x),        y, day(x))
+set(x::Date, ::typeof(day),                     y) = Date(year(x), month(x),      y)
+set(x::Date, ::typeof(yearmonth),    y::NTuple{2}) = Date(y...,              day(x))
+set(x::Date, ::typeof(monthday),     y::NTuple{2}) = Date(year(x),             y...)
+set(x::Date, ::typeof(yearmonthday), y::NTuple{3}) = Date(y...)
+set(x::Date, ::typeof(dayofweek),               y) = firstdayofweek(x) + Day(y - 1)
+
+set(x::Time, ::typeof(hour),        y) = Time(y,       minute(x), second(x), millisecond(x), microsecond(x), nanosecond(x))
+set(x::Time, ::typeof(minute),      y) = Time(hour(x),         y, second(x), millisecond(x), microsecond(x), nanosecond(x))
+set(x::Time, ::typeof(second),      y) = Time(hour(x), minute(x),         y, millisecond(x), microsecond(x), nanosecond(x))
+set(x::Time, ::typeof(millisecond), y) = Time(hour(x), minute(x), second(x),              y, microsecond(x), nanosecond(x))
+set(x::Time, ::typeof(microsecond), y) = Time(hour(x), minute(x), second(x), millisecond(x),              y, nanosecond(x))
+set(x::Time, ::typeof(nanosecond),  y) = Time(hour(x), minute(x), second(x), millisecond(x), microsecond(x),             y)
+
+set(x::DateTime, optic::Union{typeof.((year, month, day, yearmonth, monthday, yearmonthday, dayofweek))...}, y) = modify(d -> set(d, optic, y), x, Date)
+set(x::DateTime, optic::Union{typeof.((hour, minute, second, millisecond))...}, y) = modify(d -> set(d, optic, y), x, Time)
