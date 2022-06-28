5fb59f4ab88ba26f39b7fc9ccca84c207b70fd06
Jan Weidner <jw3126@gmail.com>
2022-06-28 08:29:46 +0200

    run doctests only for julia >= 1.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,8 +15,7 @@ include("test_setindex.jl")
 include("test_functionlenses.jl")
 
 using Documenter: doctest
-if Base.thisminor(VERSION) == v"1.6"
-    # ⨟ needs to be defined
+if Base.thisminor(VERSION) >= v"1.7"
     doctest(Accessors)
 else
     @info "Skipping doctests, on VERSION = $VERSION"
