18f8f69d253219758ef9a0e77d74b71eaa6d1fc4
Mason Protter <mason.protter@icloud.com>
2024-02-27 15:16:38 +0100

    define error hint for `insert` on `DynamicIndexLens`.

--- src/Accessors.jl
+++ src/Accessors.jl
@@ -2,7 +2,7 @@ module Accessors
 using MacroTools
 using MacroTools: isstructdef, splitstructdef, postwalk
 using InverseFunctions
-
+using Markdown: Markdown, @md_str, term
 
 if !isdefined(Base, :get_extension)
     using Requires
@@ -24,5 +24,33 @@ include("testing.jl")
 include("../ext/AccessorsDatesExt.jl")
 include("../ext/AccessorsLinearAlgebraExt.jl")
 include("../ext/AccessorsTestExt.jl")
+
+function __init__()
+    if isdefined(Base.Experimental, :register_error_hint)
+        Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
+            if exc.f === insert && argtypes[2] <: Accessors.DynamicIndexLens
+                println(io)
+                term(io, md"""
+                   `insert` with a `DynamicIndexLens` is not supported, this can happen when you write
+                   code such as `@insert a[end] = 1` or `@insert a[begin] = 1` since `end` and `begin`
+                   are functions of `a`. The reason we do not support these with `insert` is that 
+                   Accessors.jl tries to guarentee that `f(insert(obj, f, val)) == val`, but 
+                   `@insert a[end] = 1` and `@insert a[begin] = 1` will violate that invariant.
+                   
+                   Instead, you can use `first` and `last` directly, e.g.
+                   ```
+                   julia> a = (1, 2, 3, 4)
+                   
+                   julia> @insert last(a) = 5
+                   (1, 2, 3, 4, 5)
+                   
+                   julia> @insert first(a) = 0
+                   (0, 1, 2, 3, 4)
+                   ```
+                   """)
+            end
+        end
+    end
+end
 
 end
