a2b0d825d4423dd87f8155c1eb81073ce56a78dc
Alexander Plavin <alexander@plav.in>
2024-08-24 20:12:21 -0400

    set(dates): relax type signature

--- ext/AccessorsDatesExt.jl
+++ ext/AccessorsDatesExt.jl
@@ -16,9 +16,9 @@ set(x::Time, ::typeof(Dates.value), y) = @set x.instant.value = y
 set(x::Date, ::typeof(year),                    y) = Date(y,       month(x), day(x))
 set(x::Date, ::typeof(month),                   y) = Date(year(x),        y, day(x))
 set(x::Date, ::typeof(day),                     y) = Date(year(x), month(x),      y)
-set(x::Date, ::typeof(yearmonth),    y::NTuple{2}) = Date(y...,              day(x))
-set(x::Date, ::typeof(monthday),     y::NTuple{2}) = Date(year(x),             y...)
-set(x::Date, ::typeof(yearmonthday), y::NTuple{3}) = Date(y...)
+set(x::Date, ::typeof(yearmonth),    y::NTuple{2, Any}) = Date(y...,              day(x))
+set(x::Date, ::typeof(monthday),     y::NTuple{2, Any}) = Date(year(x),             y...)
+set(x::Date, ::typeof(yearmonthday), y::NTuple{3, Any}) = Date(y...)
 set(x::Date, ::typeof(dayofweek),               y) = firstdayofweek(x) + Day(y - 1)
 
 set(x::Time, ::typeof(hour),        y) = Time(y,       minute(x), second(x), millisecond(x), microsecond(x), nanosecond(x))
